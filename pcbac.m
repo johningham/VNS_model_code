273531d11c5f1b08f51f891195796da25822c491
johningham <j.f.ingham2@newcastle.ac.uk>
2024-04-11 15:09:01 +0100

    bad idea. functions removed

--- pcbac.m
+++
@@ -1,8 +0,0 @@
-
-function pcbac()
-    addpath(genpath('C:\Users\b9063306\MATLAB\Projects'))
-    addpath(genpath('C:\Users\b9063306\OneDrive - Newcastle University\PhD\VNS\Bifurcation_plots'))
-    addpath(genpath('C:\Users\b9063306\OneDrive - Newcastle University\PhD\CODE\VNS_JohnBits'))
-    cd 'C:\Users\b9063306\OneDrive - Newcastle University\PhD\CODE\VNS_JohnBits'
-
-end
