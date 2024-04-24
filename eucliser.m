792377e20311ae5a84c7537eb253058bdbb95941
johningham <j.f.ingham2@newcastle.ac.uk>
2024-04-24 18:12:33 +0100

    More formatting  and commenting

--- eucliser.m
+++ eucliser.m
@@ -1,9 +1,12 @@
 function eucvec = eucliser(u, euc_weights, FixedPoint)
+
 % can selectively calculate Euclidean distance from different combinations
 % of dimensions
+
     diffs = u - FixedPoint;
     weighted_diffs = diffs.*euc_weights;
     squares = weighted_diffs.^2;
     sums = sum(squares,2);
     eucvec = sqrt(sums);
+
 end
--- vectorised_eulersolver.m
+++ vectorised_eulersolver.m
@@ -1,19 +1,24 @@
 function [t,u] = vectorised_eulersolver(funcname,u_init,dt,tend)
 
-% the time vector is in steps of dt up to tend 
-t = 0:dt:tend;
-
-% initialise the solution u, parametersxtime
-u = zeros(length(u_init),length(t));
-u(:,1) = u_init;
-
-% euler solver
-for i = 1:length(t)-1
+% Simple solver using Euler-Maruyama for our vectorised code to run the
+% stochastic versions of the model.
     
-    dudt = funcname(t(i),u(:,i));
+    % The time vector is in steps of dt up to tend 
+    t = 0:dt:tend;
     
-    u(:,i+1) = u(:,i) + dudt*dt;
+    % initialise the solution u, parametersxtime
+    u = zeros(length(u_init),length(t));
+    u(:,1) = u_init;
     
-end   
+    % euler solver
+    for i = 1:length(t)-1
+        
+        dudt = funcname(t(i),u(:,i));
+        u(:,i+1) = u(:,i) + dudt*dt;
+        
+    end   
 
-u = u'; % transpose to match the ode output format for u.
+    u = u'; % transpose to match the ode output format for u.
+
+end
+
